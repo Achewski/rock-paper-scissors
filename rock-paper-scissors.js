c8585035bdf3ed7948a4be2c3d3ea1b9f821453e
Achewski <achewski@gmail.com>
2022-11-03 01:56:42 +1300

    Only allow valid player input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,26 +1,31 @@
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"]
-    let randomIndex = Math.floor(Math.random() * options.length);
-    let randomOption = options[randomIndex];
+    let randomOption = Math.floor(Math.random() * options.length);                                                       /* Get position of random string from options */
+        randomOption = options[randomOption];                                                                            /* Convert from position to actual string at position */
     return randomOption;
 }
 
 function playRound() {
-    let playerSelection = "Rock";
-    let computerSelection = getComputerChoice();
     let result;
     let winnerDeclaration;
-
-    (playerSelection == "Paper" && computerSelection == "Rock") ? result = "Win" :
-    (playerSelection == "Scissors" && computerSelection == "Paper") ? result = "Win" :
-    (playerSelection == "Rock" && computerSelection == "Scissors") ? result = "Win" :
-    (playerSelection == computerSelection) ? result = "Draw" :
-    result = "Lose";
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Paper, scissors, or rock?");
+        playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();           /* Force correct capitalization of player input */
     
+    while (playerSelection != "Paper" && playerSelection != "Scissors" && playerSelection != "Rock") {                  /*                                    */
+        playerSelection = prompt("Please only enter paper, scissors, or rock.");                                        /* Ensure player selects valid option */
+        playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();           /*                                    */
+    }                                                                                                                   /*                                    */
+
+    (playerSelection == "Paper" && computerSelection == "Rock") ? result = "Win" :                                      /* Player wins */
+    (playerSelection == "Scissors" && computerSelection == "Paper") ? result = "Win" :                                  /* Player wins */
+    (playerSelection == "Rock" && computerSelection == "Scissors") ? result = "Win" :                                   /* Player wins */
+    (playerSelection == computerSelection) ? result = "Draw" :                                                          /* Player and computer chose the same option */
+    result = "Lose";                                                                                                    /* Player loses */
+
     result == "Win" ? winnerDeclaration = `You Win! ${playerSelection} beats ${computerSelection}!` : 
     result == "Lose" ? winnerDeclaration = `You Lose! ${computerSelection} beats ${playerSelection}!` :
     winnerDeclaration = `It's a draw! You both chose ${playerSelection}!`;
-    
     return winnerDeclaration;
 }
 
