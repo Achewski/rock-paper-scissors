282307eb57c9ea04915e6f4b23097f89f950ee8f
Achewski <achewski@gmail.com>
2022-11-03 00:18:46 +1300

    Add playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,30 @@
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"]
+    let options = ["Rock", "Paper", "Scissors"]
     let randomIndex = Math.floor(Math.random() * options.length);
     let randomOption = options[randomIndex];
     return randomOption;
 }
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    (playerSelection == "Paper" && computerSelection == "Rock") ? result = "Win" :
+    (playerSelection == "Scissors" && computerSelection == "Paper") ? result = "Win" :
+    (playerSelection == "Rock" && computerSelection == "Scissors") ? result = "Win" :
+    (playerSelection == computerSelection) ? result = "Draw" :
+    result = "Lose";
+
+    let winnerDeclaration;
+    
+    result == "Win" ? winnerDeclaration = `You Win! ${playerSelection} beats ${computerSelection}!` : 
+    result == "Lose" ? winnerDeclaration = `You Lose! ${computerSelection} beats ${playerSelection}!` :
+    winnerDeclaration = `It's a draw! You both chose ${playerSelection}!`;
+    return winnerDeclaration;
+}
+
+console.log(playRound(playerSelection, computerSelection));
